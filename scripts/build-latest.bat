0c836aedc957b8e82d80d68278c343695cd9e32e
Josh Wade <josh@example.com>
2026-02-27 12:33:30 -0600

    Launch app on script run

--- scripts/build-latest.bat
+++ scripts/build-latest.bat
@@ -8,5 +8,15 @@ echo Building ParametricMidiSequencer.sln...
 dotnet build ".\ParametricMidiSequencer.sln"
 set "EXIT_CODE=%ERRORLEVEL%"
 
+if not "%EXIT_CODE%"=="0" (
+	echo Build failed. UI will not launch.
+	popd >nul
+	exit /b %EXIT_CODE%
+)
+
+echo Launching Parametric MIDI Sequencer UI...
+dotnet run --project ".\ui\ParametricMidiSequencer.UI.csproj"
+set "EXIT_CODE=%ERRORLEVEL%"
+
 popd >nul
 exit /b %EXIT_CODE%
